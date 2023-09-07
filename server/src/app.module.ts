93c4b599b5d611d2d2db66539b6cabec3a2491d5
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2023-09-07 17:34:29 -0600

    feat(server): :fire: connect back with database

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,19 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'test',
-      entities: [],
-      synchronize: true,
-    }),
+      host: process.env.HOST,  
+      port: parseInt(process.env.PORT),
+      database: process.env.MYSQL_DATABASE,
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+    })
   ],
 })
 export class AppModule {}
