f3634c82b3cc0e4eb5fe02b09f267ab28461a3e2
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2023-09-07 19:28:03 -0600

    feat(server): :art: change main configuration to connect with db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,19 +1,27 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configure } from './config/configure';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.HOST,  
-      port: parseInt(process.env.PORT),
-      database: process.env.MYSQL_DATABASE,
-      username: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-    })
+    ConfigModule.forRoot({
+      load: [configure],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('HOST'),
+        port: +configService.get('PORT'),
+        username: configService.get('MYSQL_USER'),
+        password: configService.get('MYSQL_PASSWORD'),
+        database: configService.get('MYSQL_DATABASE'),
+        entities: [],
+      }),
+      inject: [ConfigService],
+      imports: [ConfigModule],
+    }),
   ],
 })
 export class AppModule {}
---
+++ server/src/config/configure.ts
@@ -0,0 +1,7 @@
+export const configure = () => ({
+    host: process.env.HOST,  
+    port: parseInt(process.env.PORT),
+    database: process.env.MYSQL_DATABASE,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+})
